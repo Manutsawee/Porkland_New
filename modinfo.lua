73a8348c9f9fb2523b77ec5bc575b4627f1342c0
Jerry <2234834467@qq.com>
2023-08-08 18:46:31 +0800

    Remove translation option

--- modinfo.lua
+++ modinfo.lua
@@ -34,24 +34,4 @@ end
 
 configuration_options = {
     Breaker("Misc.", "杂项"),
-    {
-        name = "locale",
-        label = en_zh("Translation", "翻译"),
-        hover = en_zh("Select a translation to enable it regardless of language packs.", "选择翻译，而不是自动"),
-        options =
-        {
-            {description = "Auto", data = false},
-            {description = "Deutsch", data = "de"},
-            {description = "Español", data = "es"},
-            {description = "Français", data = "fr"},
-            {description = "Italiano", data = "it"},
-            {description = "한국어", data = "ko"},
-            {description = "Polski", data = "pl"},
-            {description = "Português", data = "pt"},
-            {description = "Русский", data = "ru"},
-            {description = "中文 (简体)", data = "sc"},
-            {description = "中文 (繁体)", data = "tc"},
-        },
-        default = false,
-    }
 }
