45fbb1bb48f3893501e594f3a6e914561cccb96a
ooyiyi <731152966@qq.com>
2023-08-14 13:57:29 +0000

    Update generic.lua

--- strings/generic.lua
+++ strings/generic.lua
@@ -46,6 +46,5 @@ return {
         TREE_PILLAR = "It totally blocks out the sun.",
         WEEVOLE = "Whee!",
         WEEVOLE_CARAPACE = "I'll be careful with this carapace.",
-        IRON = "Isn't it ironic?",
     },
 }
