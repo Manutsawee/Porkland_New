3edb65d2a478bd8fd8b13ddefb738b65be116ba1
ooyiyi <731152966@qq.com>
2023-08-14 14:02:49 +0000

    Update waxwell.lua

--- strings/waxwell.lua
+++ strings/waxwell.lua
@@ -46,6 +46,5 @@ return {
         TREE_PILLAR = "This tree is unusually large.",
         WEEVOLE = "Interminable pests.",
         WEEVOLE_CARAPACE = "Ugh.",
-        IRON = "A solid mineral.",
     },
 }
