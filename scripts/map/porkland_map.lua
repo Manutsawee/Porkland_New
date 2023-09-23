8ef3cf88da0a2095f8100ce09d8b59d7fafe7168
Manutsawee <manutsawee.louis@outlook.com>
2023-09-23 12:38:25 +0800

    add pangolden custom option

--- scripts/map/porkland_map.lua
+++ scripts/map/porkland_map.lua
@@ -21,6 +21,7 @@ TRANSLATE_TO_PREFABS["peagawk_spawner"] = {"peagawk_spawner"}
 TRANSLATE_TO_PREFABS["grass_tall_patch"] = {"grass_tall_patch"}
 TRANSLATE_TO_PREFABS["grass_tall"] = {"grass_tall"}
 TRANSLATE_TO_PREFABS["pog"] = {"pog"}
+TRANSLATE_TO_PREFABS["pangolden"] = {"pangolden"}
 TRANSLATE_TO_PREFABS["gnatmound"] = {"gnatmound"}
 TRANSLATE_TO_PREFABS["dungbeetle"] = {"dungbeetle"}
 TRANSLATE_TO_PREFABS["dungpile"] = {"dungpile"}
